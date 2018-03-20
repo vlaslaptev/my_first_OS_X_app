20ac96004e2e38129a03ec85440692819dfde8db
v.laptev <v.laptev@qiwi.com>
2018-03-20 11:08:49 +0300

    Initial Commit

---
+++ test_macosx_app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  test_macosx_app
+//
+//  Created by v.laptev on 19.03.2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ test_macosx_app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test_macosx_app
+//
+//  Created by v.laptev on 19.03.2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
