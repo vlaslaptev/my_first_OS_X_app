ddbf4f2a6356dcfa71bc5bf78edbb5a427783c95
v.laptev <v.laptev@qiwi.com>
2018-03-21 00:28:34 +0300

    add today  today extension

---
+++ test/ListRowViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListRowViewController.swift
+//  test
+//
+//  Created by v.laptev on 21.03.2018.
+//  Copyright © 2018 test. All rights reserved.
+//
+
+import Cocoa
+
+class ListRowViewController: NSViewController {
+
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("ListRowViewController")
+    }
+
+    override func loadView() {
+        super.loadView()
+
+        // Insert code here to customize the view
+    }
+
+}
